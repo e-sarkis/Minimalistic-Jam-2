9b53fbdccb7513c5e13071d9692c7bb77b1fe04f
Elisha Sarkis <elisha.sarkis96@gmail.com>
2017-12-23 13:01:54 -0500

    Add facing joystick direction on Z-axis

--- Minimalistic Jam 2/Assets/_Scripts/PlayerMovementController.cs
+++ Minimalistic Jam 2/Assets/_Scripts/PlayerMovementController.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementController : MonoBehaviour {
+/// <summary>
+/// Controls Player Movement as Derived from respective Controller Input
+/// </summary>
+public class PlayerMovementController : MonoBehaviour 
+{
+	private Vector2 axisInputDirection;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		axisInputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		axisInputDirection.Normalize();
+		Debug.DrawRay(Vector3.zero, axisInputDirection, Color.white, 1);
+
+		float zRot = Mathf.Atan2(axisInputDirection.x, -axisInputDirection.y) * Mathf.Rad2Deg;
+		if (axisInputDirection != Vector2.zero) transform.rotation = Quaternion.Euler(new Vector3(0, 0, zRot - 90f));
 	}
 }
