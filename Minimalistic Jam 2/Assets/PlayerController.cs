f72e01832e9b3127947b521a6289f8f385847a1a
Elisha Sarkis <elisha.sarkis96@gmail.com>
2017-12-24 14:21:53 -0500

    Create PlayerController component and add to Player gameobject

---
+++ Minimalistic Jam 2/Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	bool inputStroke	= false;
+	bool inputAim		= false;
+	bool inputFire		= false;
+
+	GameObject prefabPayload;
+
+	void Update()
+	{
+		inputAim 	= Input.GetButton("Aim");
+		inputFire	= Input.GetButtonUp("Aim");
+		inputStroke	= Input.GetButtonDown("Stroke");
+	}
+}
