576b484f6a62946b443f35463ac3195ca70b4791
Elisha Sarkis <elisha.sarkis96@gmail.com>
2017-12-24 19:11:39 -0500

    Arrows face direction of travel

--- Minimalistic Jam 2/Assets/ArrowController.cs
+++ Minimalistic Jam 2/Assets/ArrowController.cs
@@ -8,4 +8,9 @@ public class ArrowController : ProjectileController
 	{ 
 		rigidbody2D.velocity = direction * moveVelocity;
 	}
+
+	void Update()
+	{
+		 transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg, Vector3.forward);
+	}
 }
