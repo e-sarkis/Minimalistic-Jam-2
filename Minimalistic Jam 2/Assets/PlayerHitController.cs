8a547efa947f86d8e3a93c7e216efcc3e0c73b43
Elisha Sarkis <elisha.sarkis96@gmail.com>
2017-12-25 10:06:37 -0500

    Build out HitController logic for projectiles

--- Minimalistic Jam 2/Assets/PlayerHitController.cs
+++ Minimalistic Jam 2/Assets/PlayerHitController.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class PlayerHitController : MonoBehaviour 
 {
-	
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		switch (other.gameObject.tag)
+		{
+			case "Projectile":
+				ProjectileResponse(other.gameObject);
+				break;
+			default:
+				break;
+		}
+	}
+
+	void ProjectileResponse(GameObject projectile)
+	{
+		// TODO - Projectile Response code
+	}
 }
