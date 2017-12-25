39535d8dfffea603af8cf75331ee797a5eb7b72c
Elisha Sarkis <elisha.sarkis96@gmail.com>
2017-12-24 19:01:15 -0500

    Create and add ProjectileController

---
+++ Minimalistic Jam 2/Assets/ProjectileController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour 
+{
+	public GameObject shooter;	// The Player who fired this projectile
+	public GameObject target;	// The Target of this projectile
+	virtual public void Launch() {  }
+}
