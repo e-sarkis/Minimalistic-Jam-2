e3463016fb74998cfd4347cb9e48c2e3fc2afc34
Elisha <elisha.sarkis96@gmail.com>
2017-12-28 13:40:34 -0500

    Build out GameController state of the game variables and functions

--- Minimalistic Jam 2/Assets/GameController.cs
+++ Minimalistic Jam 2/Assets/GameController.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour 
 {
+	// Singleton
 	private static GameController _instance;
     public static GameController Instance { get { return _instance; } }
-
 	// Joystick Inputs
-	public enum Joystick { Joy1, Joy2, Joy3, Joy4 };
-	private Dictionary<Joystick, string> _joyEnumToJoyInputStrings;
+	public enum Joystick { Joy1, Joy2 };	// Can scale if Inputs exist in InputManager
+	private Dictionary<Joystick, string> _joyEnumToJoyInputStrings; // e.x. "Joy1" in "Joy1Horizontal"
+	// Game State Variables
+	public int totalInGamePlayers 	= 2;	// Total # Players In-Game, dead or alive
+	private const int _maxPlayers 	= 2; 	// Max # Players that can play In-Game
+	private const int _minPlayers 	= 2; 	// Min # Players required to play
+	private Dictionary<int, int> _playerNumsToScores; 
 
     private void Awake()
     {
 		// Singleton Initialization
         if (_instance != null && _instance != this) { Destroy(this.gameObject); }
 		else { _instance = this; }
-
 		// Joystick Input Initialization
 		_joyEnumToJoyInputStrings = new Dictionary<Joystick, string>();
 		_joyEnumToJoyInputStrings.Add(Joystick.Joy1, "Joy1");
 		_joyEnumToJoyInputStrings.Add(Joystick.Joy2, "Joy2");
+		// Game State Variable Initialization
+		Mathf.Clamp(totalInGamePlayers, _minPlayers, _maxPlayers);
+		_playerNumsToScores = new Dictionary<int, int>();
+		for (int i = 0; i < totalInGamePlayers; i++) _playerNumsToScores.Add(i, 0);
     }
 
 	public string GetJoystickInputString(Joystick joy)
 	{
 		return _joyEnumToJoyInputStrings[joy];
+	}
+
+	public void Score(int playerNum)
+	{
+		_playerNumsToScores[playerNum]++;
+		// TODO - Update ingame UI of Player in question
 	}
 }
