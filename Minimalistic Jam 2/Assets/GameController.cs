208fdd86a82b8673423b1c306d15b918786e6416
Elisha <elisha.sarkis96@gmail.com>
2017-12-28 12:57:53 -0500

    Build out singleton GameController script

--- Minimalistic Jam 2/Assets/GameController.cs
+++ Minimalistic Jam 2/Assets/GameController.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour 
+{
+	private static GameController _instance;
+    public static GameController Instance { get { return _instance; } }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Awake()
+    {
+        if (_instance != null && _instance != this) { Destroy(this.gameObject); }
+		else { _instance = this; }
+    }
 }
