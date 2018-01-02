97b53ed45a75ba653ceacc82afa4a5082fe6f8f7
Elisha Sarkis <elisha.sarkis96@gmail.com>
2018-01-01 19:19:21 -0500

    Add application closing functionality

---
+++ Minimalistic Jam 2/Assets/ApplicationQuitter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationQuitter : MonoBehaviour 
+{
+	void Awake() { DontDestroyOnLoad(gameObject); }
+
+	void Update () 
+	{
+		if (Input.GetButtonDown("Cancel"))
+		{
+			Application.Quit();
+		}
+	}
+}
